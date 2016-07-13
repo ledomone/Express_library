c63f3ee9403e47941fdc494db3b421ca661622fb
ledomone <ledom.git@gmail.com>
2016-07-13 22:22:35 +0200

    Jade (templating engine)

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-    res.send('Hello world!');
+    res.render('index');
 });
 
 app.get('/books', function (req, res) {
