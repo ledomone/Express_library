b494a534afd5fb3140dbfb27689defcf4d5f3c06
Dominik <ledom.git@gmail.com>
2016-07-14 15:55:08 +0200

    Separate Files (routes)

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app = express();
 
 var port = process.env.PORT || 5000;
 
-var bookRouter = express.Router();
+var bookRouter = require('./src/routes/bookRoutes');
 
 app.use(express.static('public'));
 app.set('views', './src/views');
@@ -21,66 +21,6 @@ var nav = [{
     Link: '/Authors',
     Text: 'Authors'
             }];
-
-var books = [
-    {
-        title: 'War and Peace',
-        genre: 'Historical Fiction',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-},
-    {
-        title: 'Les Misérables',
-        genre: 'Historical Fiction',
-        author: 'Victor Hugo',
-        read: false
-},
-    {
-        title: 'The Time Machine',
-        genre: 'Science Fiction',
-        author: 'H. G. Wells',
-        read: false
-},
-    {
-        title: 'A Journey into the Center of the Earth',
-        genre: 'Science Fiction',
-        author: 'Jules Verne',
-        read: false
-},
-    {
-        title: 'The Dark World',
-        genre: 'Fantasy',
-        author: 'Henry Kuttner',
-        read: false
-},
-    {
-        title: 'The Wind in the Willows',
-        genre: 'Fantasy',
-        author: 'Kenneth Grahame',
-        read: false
-},
-    {
-        title: 'Life On The Mississippi',
-        genre: 'History',
-        author: 'Mark Twain',
-        read: false
-},
-    {
-        title: 'Childhood',
-        genre: 'Biography',
-        author: 'Lev Nikolayevich Tolstoy',
-        read: false
-}
-];
-
-bookRouter.route('/')
-    .get(function (req, res) {
-        res.render('books', {
-            title: 'Hello from render',
-            nav: nav,
-            books: books
-        });
-    });
 
 app.use('/Books', bookRouter);
 
