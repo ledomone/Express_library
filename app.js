cef2c57b7969268b4f6063405aa25a0ba561ed04
Dominik <ledom.git@gmail.com>
2016-07-14 13:48:44 +0200

    Add routing

--- app.js
+++ app.js
@@ -7,10 +7,19 @@ var app = express();
 
 var port = process.env.PORT || 5000;
 
+var bookRouter = express.Router();
+
 app.use(express.static('public'));
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
+
+bookRouter.route('/single')
+    .get(function (req, res) {
+        res.send('Hello Single Book');
+    });
+
+app.use('/Books', bookRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
