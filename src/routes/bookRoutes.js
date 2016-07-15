6864ebc44136f6de86f0f1bc73ed97632073ccd4
Dominik <ledom.git@gmail.com>
2016-07-15 14:40:22 +0200

    Select One

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
 var router = function (nav) {
 
@@ -23,17 +24,25 @@ var router = function (nav) {
                     });
                 });
             });
-
-
         });
 
     bookRouter.route('/:id')
         .get(function (req, res) {
-            var id = req.params.id;
-            res.render('bookView', {
-                title: 'Single Book',
-                nav: nav,
-                book: books[id]
+            var id = new objectId(req.params.id);
+            var url = 'mongodb://localhost:27017/libraryApp';
+
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+                collection.findOne({
+                    _id: id
+                }, function (err, results) {
+                    // TODO: check if not error and then:
+                    res.render('bookView', {
+                        title: 'Book',
+                        nav: nav,
+                        book: results
+                    });
+                });
             });
         });
 
