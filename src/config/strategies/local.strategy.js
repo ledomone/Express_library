751c3619fe21c76cdccb49bb550d7ae0d22e0331
Dominik <ledom.git@gmail.com>
2016-07-19 15:18:20 +0200

    Verifying the User in the Database

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -2,7 +2,8 @@
 "use strict";
 
 var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+    LocalStrategy = require('passport-local').Strategy,
+    mongodb = require('mongodb').MongoClient;
 
 module.exports = function () {
     passport.use(new LocalStrategy({
@@ -10,10 +11,20 @@ module.exports = function () {
             passwordField: 'password'
         },
         function (username, password, done) { //if it is proper sign in 
-            var user = {
-                username: username,
-                password: password
-            };
-            done(null, user);
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('users');
+                collection.findOne({
+                        username: username
+                    },
+                    function (err, results) {
+                        if (results.password === password) {
+                            var user = results;
+                            done(null, user);
+                        } else {
+                            done(null, false, {message: 'Bad password'});
+                        }
+                    });
+            });
         }));
 };
