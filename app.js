9b2901ae425b5718ea5cb67ca78bba9f1d07217b
Dominik <ledom.git@gmail.com>
2016-07-19 09:56:10 +0200

    Creating the Auth Routes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 "use strict";
 
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 5000;
@@ -17,14 +17,20 @@ var nav = [{
 
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
+// middleware
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
 
 app.get('/', function (req, res) {
     res.render('index', {
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,17 @@
+/*jslint node: true */
+"use strict";
+
+var express = require('express');
+var authRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
+
+var router = function(){
+    authRouter.route('/signUp')
+        .post(function(req, res) {
+            console.log(req.body);
+    });
+    
+    return authRouter;
+};
+
+module.exports = router;
