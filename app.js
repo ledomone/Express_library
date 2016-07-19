a6e4772da8a246d09f2a770e985b26e1ce206b02
Dominik <ledom.git@gmail.com>
2016-07-19 13:38:52 +0200

    Passport Middleware

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
+
 var app = express();
 
 var port = process.env.PORT || 5000;
@@ -23,6 +27,10 @@ var authRouter = require('./src/routes/authRoutes')(nav);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+
+require('./src/config/passport')(app);
 
 app.set('views', './src/views');
 
