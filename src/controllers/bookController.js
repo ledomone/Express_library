f58e53c8c4180269a9b2a1c1142d3ee6328ccb0e
Dominik <ledom.git@gmail.com>
2016-07-19 16:01:27 +0200

    Controllers

---
+++ src/controllers/bookController.js
@@ -0,0 +1,59 @@
+/*jslint node: true */
+"use strict";
+
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+
+var bookController = function (bookService, nav) {
+    
+    var middleware = function (req, res, next) {
+        if (!req.user) {
+             res.redirect('/'); // if you are not logged in - go back to slash / ;-)
+        }
+        next();
+    };
+    
+    var getIndex = function (req, res) {
+
+        var url = 'mongodb://localhost:27017/libraryApp';
+
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('books');
+            collection.find({}).toArray(function (err, results) {
+                // TODO: check if not error and then:
+                res.render('bookListView', {
+                    title: 'Books',
+                    nav: nav,
+                    books: results
+                });
+            });
+        });
+    };
+
+    var getById = function (req, res) {
+        var id = new objectId(req.params.id);
+        var url = 'mongodb://localhost:27017/libraryApp';
+
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('books');
+            collection.findOne({
+                _id: id
+            }, function (err, results) {
+                // TODO: check if not error and then:
+                res.render('bookView', {
+                    title: 'Book',
+                    nav: nav,
+                    book: results
+                });
+            });
+        });
+    };
+
+    return {
+        getIndex: getIndex,
+        getById: getById,
+        middleware: middleware
+    }
+};
+
+module.exports = bookController;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,55 +3,18 @@
 
 var express = require('express');
 var bookRouter = express.Router();
-var mongodb = require('mongodb').MongoClient;
-var objectId = require('mongodb').ObjectID;
+
 
 var router = function (nav) {
+    var bookController = require('../controllers/bookController')(null, nav);
 
-    bookRouter.use(function (req, res, next) {
-        if (!req.user) {
-            res.redirect('/'); // if you are not logged in - go back to slash / ;-)
-        }
-        next();
-    });
+    bookRouter.use(bookController.middleware);
 
     bookRouter.route('/')
-        .get(function (req, res) {
-
-            var url = 'mongodb://localhost:27017/libraryApp';
-
-            mongodb.connect(url, function (err, db) {
-                var collection = db.collection('books');
-                collection.find({}).toArray(function (err, results) {
-                    // TODO: check if not error and then:
-                    res.render('bookListView', {
-                        title: 'Books',
-                        nav: nav,
-                        books: results
-                    });
-                });
-            });
-        });
+        .get(bookController.getIndex);
 
     bookRouter.route('/:id')
-        .get(function (req, res) {
-            var id = new objectId(req.params.id);
-            var url = 'mongodb://localhost:27017/libraryApp';
-
-            mongodb.connect(url, function (err, db) {
-                var collection = db.collection('books');
-                collection.findOne({
-                    _id: id
-                }, function (err, results) {
-                    // TODO: check if not error and then:
-                    res.render('bookView', {
-                        title: 'Book',
-                        nav: nav,
-                        book: results
-                    });
-                });
-            });
-        });
+        .get(bookController.getById);
 
     return bookRouter;
 };
